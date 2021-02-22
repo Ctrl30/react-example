84897be239d7c5e54107b64e587e0ae679d1229c
logan <shaoxiong_xu@ziguangcn.com>
2021-02-22 14:28:30 +0800

    test

--- src/pages/useLayoutEffect/index.jsx
+++ src/pages/useLayoutEffect/index.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useLayoutEffect} from 'react';
+import React,{useLayoutEffect} from 'react';
 import style from './style.scss';
 
 export default function UseLayoutEffect() {
